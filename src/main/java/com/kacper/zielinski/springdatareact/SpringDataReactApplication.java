665aaf419ab03c157622a1193e0c4e2a577d860e
Kacper Zielinski <236698@student.pwr.edu.pl>
2019-03-11 22:02:12 +0100

    Initial commit

---
+++ src/main/java/com/kacper/zielinski/springdatareact/SpringDataReactApplication.java
@@ -0,0 +1,13 @@
+package com.kacper.zielinski.springdatareact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataReactApplication.class, args);
+    }
+
+}
